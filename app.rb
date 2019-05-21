821f9a4a4ba2a9bde4eda53beb63ddfe29102af3
Alison Reilly <reillyamr@gmail.com>
2019-05-21 15:43:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ class App < Sinatra::Base
     erb :index 
   end 
   
+  post '/checkout' do 
+    @checkout = params["item"]
+  end 
+  
   configure do 
     enable :sessions
     set :session_secret, "Graycie" #find sinatra documentaion for setting this to a secure random #number generator value and then sharing that secret via environment variables in the shell to 
